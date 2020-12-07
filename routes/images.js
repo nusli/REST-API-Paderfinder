019ec05f7e387699d00d3f53c1d32db3a9c398c5
its4neilz <nils.koppe00@gmail.com>
2020-12-07 18:36:35 +0100

    bildupload

--- routes/images.js
+++ routes/images.js
@@ -16,6 +16,7 @@ const connect = mongoose.createConnection(process.env.DATABASE_URL, { useNewUrlP
 var path = require('path');
 
 
+
 var mongoDriver = mongoose.mongo;
 var gfs;
 
@@ -44,31 +45,10 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
-router.post('/upload', (req, res) => {
-    // 'profile_pic' is the name of our file input field in the HTML form
-    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
 
-    upload(req, res, function(err) {
-        // req.file contains information of uploaded file
-        // req.body contains information of text fields, if there were any
 
-        if (req.fileValidationError) {
-            return res.send(req.fileValidationError);
-        }
-        else if (!req.file) {
-            return res.send('Please select an image to upload');
-        }
-        else if (err instanceof multer.MulterError) {
-            return res.send(err);
-        }
-        else if (err) {
-            return res.send(err);
-        }
 
-        // Display uploaded image for user validation
-        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
-    });
-});
+
 
 // Getting all
 router.get('/', 
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const Stamm = require('./models/stamm')
 
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
 // initialize passport
 // TODO
 
@@ -34,6 +37,7 @@ initializePassport(
         return null;
     }}
 ) 
+
 
 app.use(express.urlencoded({extended: false}))
 app.use(flash())
@@ -163,5 +167,22 @@ function checkAuthenticated(req, res, next) {
     }
     return res.redirect('login')
 }*/
+
+app.post('/upload', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.profile_pic;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('images/' + sampleFile.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});  
 
 app.listen(3000, () => console.log('Server has started'))

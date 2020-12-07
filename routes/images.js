0184264cf00857b8db7b623e54765146a510a689
Brennecke <linus.brennecke@atos.net>
2020-12-07 12:35:48 +0100

    storing images

--- routes/images.js
+++ routes/images.js
@@ -2,20 +2,84 @@ const express = require('express')
 const router = express.Router()
 const Image = require('../models/image')
 
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+
+// got the code from here:
+// https://www.youtube.com/watch?v=EVIGIcm7o2w
+const mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+var Gridfs = require('gridfs-stream');
+// establish mongodb connection
+//mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+const connect = mongoose.createConnection(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+var path = require('path');
+
+
+var mongoDriver = mongoose.mongo;
+var gfs;
+
+connect.once('open', () => {
+    // initialize stream
+    gfs = new mongoose.mongo.GridFSBucket(connect.db);
+});
+
 // Getting all
-router.get('/', async (req, res) => {
-    try{
-        const images = await Image.find();
-        res.json(images);
-    } catch (err){
-        res.status(500).json({ message: err.message });
-    }
+router.get('/', 
+    (req, res, next) => {
+        gfs.find().toArray((err, files) => {
+            if (err) {
+                console.log(err);
+            return res.status(400).json({
+                success: false,
+                message: err.message
+            });
+            }
+            else if (!files || files.length === 0) {
+                return res.status(200).json({
+                    success: false,
+                    message: 'No files available'
+                });
+            }
+
+            files.map(file => {
+                if (file.contentType === 'image/jpeg' || file.contentType === 'image/png' || file.contentType === 'image/svg') {
+                    file.isImage = true;
+                } else {
+                    file.isImage = false;
+                }
+            });
+
+            res.status(200).json({
+                success: true,
+                files,
+            });
+        });
 })
 // Getting one
-router.get('/:id', getImage, (req, res) => {
-    res.contentType(res.img.contentType);
-    res.send(res.img.data);
-})
+router.get('/:metadata', (req, res, next) => {
+    gfs.find({metadata: req.params.metadata}).toArray((err, files) => {
+        console.log("files", files)
+        if (err) {
+            console.log(err);
+            return res.status(400).json({
+                success: false,
+                message: err.message
+            });
+        }
+        else if (!files[0] || files.length === 0) {
+            return res.status(200).json({
+                success: false,
+                message: 'No files available',
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            file: files[0],
+        });
+    });
+});
 // creating one
 router.post('/', async (req, res) => {
     // required fields
--- routes/posts.js
+++ routes/posts.js
@@ -9,45 +9,55 @@ const Image = require('../models/image')
 var nodeUuid = require('node-uuid');
 const fs = require('fs');
 
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+
 // got the code from here:
 // https://www.youtube.com/watch?v=EVIGIcm7o2w
 const mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 var Gridfs = require('gridfs-stream');
 // establish mongodb connection
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+//mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+const connect = mongoose.createConnection(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 var path = require('path');
 
 
 var mongoDriver = mongoose.mongo;
 var gfs;
 
+connect.once('open', () => {
+    // initialize stream
+    gfs = new mongoose.mongo.GridFSBucket(connect.db);
+});
+/*
 mongoose.connection.once('open', function () {
     
     gfs = Gridfs(mongoose.connection.db, mongoDriver);
    
     // all set!
   })
-
-
-/* gridfs
-// require GridFS
-const Grid = require('gridfs-stream');
-const fs=require('fs');
-
-// connect GridFS and Mongo
-Grid.mongo = mongoose.mongo
 */
+
+// create storage engine
+const storage = new GridFsStorage({
+    url: process.env.DATABASE_URL,
+    file: (req, file) => {
+        console.log(req.body)
+        console.log(file)
+        return {
+            filename: file.originalname,
+            metadata: req.body.titel
+        }
+    }
+});
+
+const upload = multer({ storage });
 
 // Getting all
 router.get('/', async (req, res) => {
     try{
         const posts = await Post.find();
-        for (p of posts) {
-            if (p.image_id != null) {
-                p.image = (await getImage(p.image_id)).img
-            } 
-        }
         res.json(posts);
     } catch (err){
         res.status(500).json({ message: err.message });
@@ -55,10 +65,12 @@ router.get('/', async (req, res) => {
 })
 // Getting one
 router.get('/:id', getPost, (req, res) => {
+    console.log("post", res.post)
     res.json(res.post)
 })
 // creating one
-router.post('/', async (req, res) => {
+router.post('/', upload.single('image'), async (req, res) => {
+    //console.log("post request ", req.file, req.body)
     // required fields
     if (req.body.titel == null) return res.status(400 /* wrong user input */).json({message: '"titel" is missing.'})
     const post = new Post({
@@ -69,14 +81,14 @@ router.post('/', async (req, res) => {
     // optional fields
     if (req.body.stamm_id != null) post.stamm_id = req.body.stamm_id
     if (req.body.autor != null) post.autor = req.body.autor
+    if (req.body.art != null) post.art = [req.body.art]
     if (req.body.inhalt != null) post.inhalt = req.body.inhalt
     if (req.body.tags != null) post.tags = req.body.tags
-    if (req.body.image != null) {
+    if (req.file != null) {
+        console.log("file", req.file)
+        post.image_id = req.file.id;
 
-        var uuid = nodeUuid.v4();
-        post.image_id = uuid;
-
-        
+        /*
         try {
             console.log(req.body.image.name)
             var writeStream = gfs.createWriteStream({
@@ -90,9 +102,9 @@ router.post('/', async (req, res) => {
 
         } catch (err) {
             console.log("could not save image", err.message);
-            res.status(400 /* wrong user input */).json({message: err.message});
+            res.status(400 /* wrong user input *//*).json({message: err.message});
             return;
-        }
+        }*/
 
     }
     
@@ -139,18 +151,28 @@ router.delete('/:id', getPost, async (req, res) => {
     }
 })
 
-async function getImage(image_id) {
+function getImage(image_id) {
     try {
-        var image;
-        var readstream = gfs.createReadStream({
-            _id: '50e03d29edfdc00d34000001'
-        })
-        readstream.pipe(image);
+        /*
+        await gfs.find().toArray((err, files) => {
+            console.log("files", files)
+            if (!files[0] || files.length === 0) {
+                console.log(err)
+                console.log("no image found", image_id)
+                image= null;
+            }
+
+            let image = files[0];
+
+            return image
+
+        }); */
+        console.log("find")
+        return gfs.find().toArray();
     }
     catch (err) {
         console.log('couldnt retrieve image', image_id, err.message)
     }
-    return image
 }
 
 async function getPost(req, res, next) {
@@ -159,14 +181,28 @@ async function getPost(req, res, next) {
         if (post == null){
             return res.status(404).json({message: 'Post konnte nicht gefunden werden.'})
         }
-        if(post.image_id != null) {
-            post.image = (await getImage(post.image_id)).img
-        }
+        
     } catch (err) {
         return res.status(500).json( { message: err.message})
     }
     res.post = post
-    next()
+/*
+    if(res.post.image_id != null) {
+        gfs.find({metadata: res.post.titel}).toArray( (err, files) => {
+            if (err) console.log(err);
+            if (!files || files.length === 0) {
+                console.log("Couldn't retrieve file", res.post.image_id)
+            }
+            res.post.image = files[0]
+            res.post.image = 14
+            console.log(res.post)
+            next()
+        })
+        
+    }
+    else {*/
+        next()
+    //}  
 }
 
 module.exports = router;
